f4de614196f16de7527726601c395725ce846f0a
sarafat27 <sarafat2769@gmail.com>
2022-04-25 01:16:25 +0600

    pagination system added by counting data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
+require('dotenv').config()
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 
@@ -12,13 +13,32 @@ app.use(express.json())
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.amvjg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("emaJohn").collection("product");
-    // perform actions on the collection object
-    console.log('mongo is connected')
-    client.close();
-});
 
+async function run() {
+    try {
+        await client.connect();
+        const productCollection = client.db('emaJohn').collection('product');
+
+        app.get('/product', async (req, res) => {
+            const query = {}
+            const cursor = productCollection.find(query)
+            const products = await cursor.toArray()
+            res.send(products)
+        });
+
+        app.get('/productCount', async (req, res) => {
+            const query = {}
+            const cursor = productCollection.find(query);
+            const count = await cursor.count();
+            res.send({ count })
+        })
+    }
+    finally {
+
+    }
+}
+
+run().catch(console.dir)
 
 app.get('/', (req, res) => {
     res.send('server is running')
